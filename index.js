2fa2ee903bd467d6eb2c2daa8f77155a148d7390
naeem5842 <101958773+naeem5842@users.noreply.github.com>
2022-05-26 11:21:42 +0530

    get method initialized

--- index.js
+++ index.js
@@ -6,12 +6,29 @@ const app = express();
 
 app.get("/", function(req, res){
 
-        const url ="https://api.openweathermap.org/data/2.5/weather?zip=94040,us&appid=5ef7ad0fede34fc42546659a92e8f5fa&units=metric";
+        const url ="https://api.openweathermap.org/data/2.5/weather?q=satara&appid=5ef7ad0fede34fc42546659a92e8f5fa&units=metric";
 
         https.get(url , function(response){
-            console.log(response);
+            console.log(response.statusCode);
+            response.on("data", function(data){
+                const weatherData = JSON.parse(data);
+                // console.log(weatherData);
+                const temp = weatherData.main.temp;
+                console.log(temp);
+                const description =weatherData.weather[0].description;
+                console.log(description);
+                const icon = weatherData.weather[0].icon;
+                const imgurl = "http://openweathermap.org/img/wn/"+icon+"@2x.png";
+
+                
+                res.write("<p>The weather is "+ description +"</p>");
+                res.write("<h1>The temprature is " + temp + "C</h1>");
+                res.write("<img src = "+imgurl+ ">");
+                res.send();
+            });
+            
         });
-        res.sendFile(__dirname+"/index.html");
+        // res.sendFile(__dirname+"/index.html");
 });
  
 
